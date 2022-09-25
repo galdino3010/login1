a991df0f59793ecbb4e56fec6872e6e1289d8993
Antonio Carlos dos Santos <113218040+galdino3010@users.noreply.github.com>
2022-09-25 12:18:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function ValidateEmail(mail) {
+let email = document.getElementById('Email').Value;
+
+let password = document.getElementById('password').value;
+
+if(email =='antonio@gmail.com'&& password == 'carlos')
+{
+   alert('Login Successfuly!!');
+} else {
+alert('Login Failed');
+}
+}
+
